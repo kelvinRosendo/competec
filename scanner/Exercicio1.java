988f9e14d9de8e84349434f688f599f7d7434664
Kelvin_iDev <109925058+kelvinRosendo@users.noreply.github.com>
2024-04-20 11:31:47 -0300

    Update Exercicio1.java

--- scanner/Exercicio1.java
+++ scanner/Exercicio1.java
@@ -1,7 +1,67 @@
-public class Exercicio1 {
-    public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
+import java.util.Scanner;
+import java.util.*;
 
-
-    }
+public class Main
+{
+	public static void main(String[] args) {
+	 
+	  /*System.out.println("Qual o seu nome, novo comprador");
+	  var sc = new Scanner(System.in);
+	  var nome = sc.nextLine();
+	  System.out.println("Bem Vindo ao nosso mercado " + nome);
+	  
+	  var kl = new Scanner(System.in);
+	  
+	  System.out.println ("Quantos abacaxis você vai levar, " + nome);
+	  int abx = kl.nextInt();
+      
+      System.out.println ("E quantas maçãs você vai levar? ");
+      int mca = kl.nextInt();
+      
+      System.out.println ("E quantas peras você vai levar ?");
+      int pra = kl.nextInt();
+      
+      double compValor = 7.30*abx + 2.00*mca + 3.00*pra;
+	
+	  System.out.println ("O valor total da sua compra deu: R$" + compValor);
+	
+	  var nc = new Scanner(System.in);
+	  System.out.println("Qual o seu nome?");
+	  String a = nc.nextLine();
+	  
+	  var ncB = new Scanner(System.in);
+	  System.out.println("E o nome do seu amigo?");
+	  int b = nc.nextInt();
+	  
+	  System.out.println(a.equals(b));
+	  
+	   var nm = new Scanner(System.in);
+	  System.out.println("Qual a sua idade?");
+	  int c = nm.nextInt();
+	  
+	  var nmB = new Scanner(System.in);
+	  System.out.println("E a idade do seu professor?");
+	  int d = nm.nextInt();
+	  
+	  System.out.println(c<d);*/
+	  
+	  var chuvenu = true;
+	  var repangalejando = true;
+	  
+	  if (chuvenu == true && repangalejando == true)
+	        System.out.println("Não sair de casa hoje, partiu assistir série");
+	  else
+	    System.out.println("Vamos dar um rolê");
+	  
+	  var macarrao = true;
+	  var estrogonofe = true;
+	  
+	  if (macarrao == true || estrogonofe == true)
+	        System.out.println("Hoje eu forro o estômago Legal");
+	  else
+	    System.out.println("triseza, apenas");
+	  
+	  
+	  
+	}
 }
